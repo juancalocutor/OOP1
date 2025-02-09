bf8e0a63809b443461b4d7f593c22c1d81d7f3ed
juancalocutor <juancalocutor@gmail.com>
2025-02-08 23:44:14 -0500

    POO calse 2

--- src/Clase01_01_Automovil.java
+++ src/Clase01_01_Automovil.java
@@ -1,14 +1,23 @@
 public class Clase01_01_Automovil {
 
-    String marca;
-    String modelo;
-    String anio;
-    double cilindraje;
-    String color;
-    int tanque = 40;
+    private String marca, modelo, anio, color;
+    private double cilindraje;
+    private int tanque;
+
+    /* Primero hicimos un Get y Set y al final se diseñó un constructor 
+     * la idea es que sean menos líneas de código
+    */
+
+    public Clase01_01_Automovil(String marca, String modelo, String anio, String color, double cilindraje, int tanque) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anio = anio;
+        this.color = color;
+        this.cilindraje = cilindraje;
+        this.tanque = tanque;
+    }
 
     public String detalle() {
-
         return "Fabricante = " + this.marca + "\nModelo = " + this.modelo +
                 "\nAño = " + this.anio + "\nCilindraje = " + this.cilindraje +
                 "\nColor = " + this.color;
@@ -24,11 +33,29 @@ public class Clase01_01_Automovil {
 
     // sobre carga de método
 
-    public float consumo (int km, float gasolina) {
-        return km/(tanque * gasolina);
+    public float consumo(int km, float gasolina) {
+        return km / (this.tanque * gasolina);
     }
 
-    public float consumo (int km, int gasolina) {
-        return km/(tanque * (gasolina / 100f));
+    public float consumo(int km, int gasolina) {
+        return km / (this.tanque * (gasolina / 100f));
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        // Con el siguiente código se compara si los dos objetos son iguales por referencia
+        if (this == obj) {
+            return true;
+        }
+        // con el siguiente código se compara si los objetos son de la misma instancia
+        if (!(obj instanceof Clase01_01_Automovil)) {
+            return false;
+        }
+        // TODO Auto-generated method stub
+        Clase01_01_Automovil a = (Clase01_01_Automovil) obj;
+        // Al comparar el objeto de la clase con uno sin atributos retornaría excepción por eso el siguiente código
+        return (this.anio != null && this.color != null && this.anio.equals(a.anio) && this.color.equals(a.color));
+    }
+
+    
 }
--- src/Clase01_Automovil.java
+++ src/Clase01_Automovil.java
@@ -3,36 +3,40 @@ public class Clase01_Automovil {
         /*Se pueden iniciar las variables por defecto desde la clase o se pueden modificar en el main
          * También se pueden crear diferentes instancias de la clase
          */
-        Clase01_01_Automovil auto = new Clase01_01_Automovil();
-        Clase01_01_Automovil mazda = new Clase01_01_Automovil();
+        Clase01_01_Automovil auto = new Clase01_01_Automovil("Chevrolet", "SAIL LTZ",
+         "2019", "Negro", 1.4, 40);
+        Clase01_01_Automovil mazda = new Clase01_01_Automovil("Mazda", "6", 
+        "2019", "Negro", 1.6, 50);
+        Clase01_01_Automovil nissan = new Clase01_01_Automovil("Nissan", "Navara", 
+        "2019", "Negro", 3.0, 60);
 
-        auto.anio = "2019";
-        auto.cilindraje = 1.4;
-        auto.color = "Negro";
-        auto.marca = "Chevrolet";
-        auto.modelo = "SAIL LTZ";
-
-        mazda.anio = "2019";
-        mazda.cilindraje = 1.6;
-        mazda.color = "Negro";
-        mazda.marca = "Mazda";
-        mazda.modelo = "6";
+        // Creamos un objeto sin atributos para compararlo y que de false sin excepción que termine la app
+        Clase01_01_Automovil carro = new Clase01_01_Automovil(null, null, 
+        null, null, 0, 0);
 
         System.out.println();
 
-        System.out.println(auto.detalle());
-        System.out.println(auto.acelerando(3000));
-        System.out.println(auto.frenar());
-        System.out.println("Kilometros por galón " + auto.consumo(300, 0.60f));
-        System.out.println("Kilometros por galón " + auto.consumo(300, 60));
+        System.out.println(auto.detalle() + "\n" + auto.acelerando(3000) + "\n" + 
+        auto.frenar() + "\nKilometros por galón " + auto.consumo(300, 0.60f) + "\nKilometros por galón " + 
+        auto.consumo(300, 60));
+    
+        System.out.println();
+
+        System.out.println(mazda.detalle() + "\n" + mazda.acelerando(3500) + "\n" + 
+        mazda.frenar() + "\nKilometros por galón " + mazda.consumo(300, 0.75f) + "\nKilometros por galón " + 
+        mazda.consumo(300, 75));
 
         System.out.println();
 
-        System.out.println(mazda.detalle());
-        System.out.println(mazda.acelerando(3500));
-        System.out.println(mazda.frenar());
-        System.out.println("Kilometros por galón " + mazda.consumo(300, 0.75f));
-        System.out.println("Kilometros por galón " + mazda.consumo(300, 75));
+        System.out.println(nissan.detalle() + "\n" + nissan.acelerando(3000) + "\n" + 
+        nissan.frenar() + "\nKilometros por galón " + nissan.consumo(300, 0.60f) + "\nKilometros por galón " + 
+        nissan.consumo(300, 60));
+
+        System.out.println();
+
+        System.out.println("El Mazda y el Nissan ¿Son iguales? " + (mazda == nissan));
+        System.out.println("El año y el color del Mazda y el Nissan ¿Son iguales? " + (mazda.equals(nissan)));
+        System.out.println("El Mazda y el carro tienen el mismo color y son del mismo año? " + (mazda.equals(carro)));
 
         System.out.println();
     }
